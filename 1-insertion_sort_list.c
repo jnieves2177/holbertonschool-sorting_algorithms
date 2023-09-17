a3e16a5a33fc385df4fe82098c1191dba3c0c706
jnieves2177 <jnieves3177@gmail.com>
2023-09-17 00:46:06 +0000

    insertion edit

--- 1-insertion_sort_list.c
+++ 1-insertion_sort_list.c
@@ -2,8 +2,8 @@
 
 /**
  * insertion_sort_list - function that sorts a doubly linked list
- * of integers in ascending order using the Insertion
- * sort algorithm
+ *                       of integers in ascending order using the Insertion
+ *                       sort algorithm
  * @list: doubly linked list
  * Return: void
  */
@@ -12,35 +12,41 @@ void insertion_sort_list(listint_t **list)
     if (list == NULL || *list == NULL || (*list)->next == NULL)
         return;
 
-    listint_t *current, *insertion_point, *temp;
-
-    current = (*list)->next;
+    listint_t *current = (*list)->next;
 
     while (current)
     {
-        insertion_point = current->prev;
+        listint_t *insertion_point = current->prev;
+        listint_t *temp = current->next;
 
         while (insertion_point && insertion_point->n > current->n)
         {
-            temp = current->next;
-
-            if (insertion_point->prev)
-                insertion_point->prev->next = current;
-            else
-                *list = current;
-
-            if (current->next)
-                current->next->prev = insertion_point;
-
-            current->prev = insertion_point->prev;
-            insertion_point->prev = current;
-            current->next = insertion_point;
-
-            print_list(*list);
-
-            current = temp;
+            insertion_point = insertion_point->prev;
         }
 
-        current = current->next;
+        if (insertion_point == NULL)
+        {
+            (*list)->prev = current;
+            current->next = *list;
+            current->prev = NULL;
+            *list = current;
+        }
+        else
+        {
+            current->prev = insertion_point;
+            current->next = insertion_point->next;
+
+            if (insertion_point->next)
+                insertion_point->next->prev = current;
+
+            insertion_point->next = current;
+        }
+
+        if (temp)
+            temp->prev = current;
+
+        current = temp;
+
+        print_list(*list);
     }
 }
