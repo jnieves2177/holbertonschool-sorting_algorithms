d907c8c727aa81f4c55218b332eddb200350a18a
jnieves2177 <jnieves3177@gmail.com>
2023-09-17 17:12:48 +0000

    header edit

--- sort.h
+++ sort.h
@@ -29,4 +29,8 @@ void shell_sort(int *array, size_t size);
 void cocktail_sort_list(listint_t **list);
 void counting_sort(int *array, size_t size);
 void merge_sort(int *array, size_t size);
+
+/* lomuto partition function */
+int lomuto_partition(int *array, int start, int end, int size);
+
 #endif
